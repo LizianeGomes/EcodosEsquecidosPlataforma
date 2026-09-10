9a3964ed199367bbcca0e8c421a32446321798e2
LizianeGomes <145374731+LizianeGomes@users.noreply.github.com>
2026-09-10 17:18:58 -0300

    lçllçl

--- plataformaede/Assets/Scripts/BotaoCreditos.cs
+++ plataformaede/Assets/Scripts/BotaoCreditos.cs
@@ -3,8 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class BotaoCreditos : MonoBehaviour
 {
-    public void IrParaCreditos()
+    public void AbrirCreditos()
     {
         SceneManager.LoadScene("Creditos");
+    }
+
+    public void VoltarAoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SairDoJogo()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
