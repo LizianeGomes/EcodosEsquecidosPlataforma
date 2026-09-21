e17d085d82ba67e346f699d7df4f609d96cf6b3d
LizianeGomes <145374731+LizianeGomes@users.noreply.github.com>
2026-09-21 17:18:14 -0300

    mnm

--- plataformaede/Assets/Scripts/InimigoController.cs
+++ plataformaede/Assets/Scripts/InimigoController.cs
@@ -1,15 +1,41 @@
 using UnityEngine;
+using System.Collections;
 
 public class InimigoController : MonoBehaviour
 {
     private bool morto = false;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     public void MorrerPisado()
     {
         if (morto) return;
 
         morto = true;
-        Destroy(gameObject); // por enquanto só some, sem animação
+
+        // Desativa colisores e movimento
+        GetComponent<EnemyAI>().enabled = false;
+
+        Collider2D[] colliders = GetComponentsInChildren<Collider2D>();
+        foreach (Collider2D col in colliders)
+            col.enabled = false;
+
+        GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+        GetComponent<Rigidbody2D>().simulated = false;
+
+        anim.SetTrigger("morto");
+
+        StartCoroutine(DestruirAposAnimacao());
+    }
+
+    IEnumerator DestruirAposAnimacao()
+    {
+        yield return new WaitForSeconds(0.6f); // ajuste pro tempo real da sua animação de morte
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
